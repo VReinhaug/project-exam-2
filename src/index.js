9bc80974d3d81dea074c00050b4d8c5d71754c9b
VReinhaug <v.reinhaug@gmail.com>
2026-03-15 10:45:07 +0100

    added styling scss files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./styles/main.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./custom.scss";
-
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
