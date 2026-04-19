d4d125f366c4837765d7604271d6752d9c91dd54
VReinhaug <v.reinhaug@gmail.com>
2026-04-19 14:15:31 +0200

    Added AuthContext

---
+++ src/auth/AuthContext/index.jsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  // Load user
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  function login(userData) {
+    localStorage.setItem("token", userData.accessToken);
+    localStorage.setItem("user", JSON.stringify(userData));
+    setUser(userData);
+  }
+
+  function logout() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setUser(null);
+
+    navigate("/");
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
