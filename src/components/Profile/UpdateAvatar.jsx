41f5f55d0423727ae034263e2905e885b6199e11
VReinhaug <v.reinhaug@gmail.com>
2026-04-21 17:51:49 +0200

    Fixed icon and toggle for updating avatar on profile

--- src/components/Profile/UpdateAvatar.jsx
+++ src/components/Profile/UpdateAvatar.jsx
@@ -3,9 +3,13 @@ import { Form, Button } from "react-bootstrap";
 import { profileUrl } from "../../api";
 import { getHeaders } from "../../auth/AuthHeaders";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+
 function UpdateAvatar({ name, onUpdate }) {
   const [url, setUrl] = useState("");
   const [alt, setAlt] = useState("");
+  const [showForm, setShowForm] = useState(false);
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -27,24 +31,42 @@ function UpdateAvatar({ name, onUpdate }) {
       onUpdate(data.data);
       setUrl("");
       setAlt("");
+      setShowForm(false);
     }
   }
 
   return (
-    <Form onSubmit={handleSubmit} className="mt-4">
-      <Form.Group>
-        <Form.Label>Update avatar URL</Form.Label>
-        <Form.Control value={url} onChange={(e) => setUrl(e.target.value)} />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Update avatar alternative text</Form.Label>
-        <Form.Control value={alt} onChange={(e) => setAlt(e.target.value)} />
-      </Form.Group>
+    <div className="avatar-edit mt-2">
+      {!showForm ? (
+        <FontAwesomeIcon
+          icon={faPenToSquare}
+          onClick={() => setShowForm(true)}
+          style={{ cursor: "pointer" }}
+          title="Edit avatar"
+        />
+      ) : (
+        <Form onSubmit={handleSubmit} className="mt-4">
+          <Form.Group>
+            <Form.Label>Update avatar URL</Form.Label>
+            <Form.Control
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Update avatar alternative text</Form.Label>
+            <Form.Control
+              value={alt}
+              onChange={(e) => setAlt(e.target.value)}
+            />
+          </Form.Group>
 
-      <Button type="submit" className="mt-2">
-        Update Avatar
-      </Button>
-    </Form>
+          <Button type="submit" className="mt-2">
+            Update Avatar
+          </Button>
+        </Form>
+      )}
+    </div>
   );
 }
 
